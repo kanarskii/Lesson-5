191b89f4835738d11585bb4a4998dfcd71a4d4e7
kanarskievgeni <kanarskiievgenii198@gmail.com>
2024-04-03 13:21:00 +0300

    version_2 fixed task3

--- src/Task3.java
+++ src/Task3.java
@@ -19,6 +19,9 @@ public class Task3 {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
+                if(array[i][j] < 10){
+                    System.out.print(" ");
+                }
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
